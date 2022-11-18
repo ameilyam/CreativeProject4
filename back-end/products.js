8cc9d0b2645885bb54d3c1098187c42807b2f841
Ubuntu <ubuntu@ip-172-31-2-50.us-west-1.compute.internal>
2022-11-18 02:21:28 +0000

    created and tested back-end

---
+++ back-end/products.js
@@ -0,0 +1,79 @@
+const products = [
+  {
+    name: "Crewneck Sweatshirts",
+    price: "$30.00",
+    image: "images/crewneckSweatshirts.jpg",
+  },
+  {
+    name: "White Ribbed Sweater",
+    price: "$40.00",
+    image: "images/ribbedWhiteSweater.jpg",
+  },
+  {
+    name: "Green Button Up Top",
+    price: "$25.00",
+    image: "images/greenButtonUp.jpg",
+  },
+  {
+    name: "Neutral Cardigans",
+    price: "$35.00",
+    image: "images/neutralCardigans.jpg",
+  },
+  {
+    name: "Classic Jeans",
+    price: "$60.00",
+    image: "images/stackOfJeans.jpg",
+  },
+  {
+    name: "Yellow Shoes",
+    price: "$20.00",
+    image: "images/yellowShoes.jpg",
+  },
+  {
+    name: "Hooded Sweatshirts",
+    price: "$35.00",
+    image: "images/hoodedSweatshirt.jpg",
+  },
+  {
+    name: "Longline Coat",
+    price: "$3.02",
+    image: "images/longlineCoat.jpg",
+  },
+  {
+    name: "Chambray Dress",
+    price: "$50.00",
+    image: "images/chambrayDress.jpg",
+  },
+  {
+    name: "Green Jacket",
+    price: "$45.00",
+    image: "images/greenMensJacket.jpg",
+  },
+  {
+    name: "Long Sleeve Shirt",
+    price: "$20.00",
+    image: "images/longSleeveShirt.jpg",
+  },
+  {
+    name: "Socks",
+    price: "$10.00",
+    image: "images/socks.jpg",
+  },
+  {
+    name: "Tan Trousers",
+    price: "$45.00",
+    image: "images/tanTrousers.jpg",
+  },
+  {
+    name: "White Quilted Jacket",
+    price: "$55.00",
+    image: "images/whiteQuiltedJacket.jpg",
+  },
+  {
+    name: "White T-shirt",
+    price: "$15.00",
+    image: "images/whiteTshirt.jpg",
+  },
+];
+
+module.exports = products;
